565e5a8780be810e788d5351f59f396e6cc67a33
DBrdak <brdak02dominik@gmail.com>
2023-07-13 19:31:47 +0200

    File Download feature

--- src/components/WeightForm.tsx
+++ src/components/WeightForm.tsx
@@ -4,6 +4,8 @@ import MyTextInput from './MyTextInput'
 import * as Yup from 'yup';
 import { FormValues, LineItem } from '../models/LineItem';
 import { Button, Container, Divider, Table } from 'semantic-ui-react';
+import FileSaver from 'file-saver';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 
 function WeightForm() {
   const [items, setItems] = useState<LineItem[]>([])
@@ -39,6 +41,124 @@ function WeightForm() {
     }
 
     resetForm()
+  }
+
+  async function createFile() {
+    const pdfDoc = await PDFDocument.create();
+
+    const page = pdfDoc.addPage();
+
+    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    page.setFont(font);
+    page.setFontSize(12);
+
+    page.setFontColor(rgb(0, 0, 0));
+
+    const table = {
+      x: 50,
+      y: page.getHeight() - 50,
+      width: page.getWidth() - 100,
+      height: page.getHeight() - 100,
+      numberOfRows: items.length + 1,
+      numberOfColumns: 6,
+      cellMargin: 5,
+      headerBackgroundColor: rgb(0.8, 0.8, 0.8),
+    };
+
+    drawTableHeader(page, table);
+
+    await drawTableRows(page, table, pdfDoc);
+
+    const pdfBytes = await pdfDoc.save();
+
+    const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' });
+
+    FileSaver.saveAs(pdfBlob, 'example.pdf');
+  }
+
+  function drawTableHeader(page: any, table: any) {
+    const { x, y, width, height, numberOfRows, numberOfColumns, cellMargin } = table;
+    const columnWidth = width / numberOfColumns;
+  
+    let currentY = y - cellMargin - 10;
+  
+    page.drawLine({
+      start: { x, y: currentY },
+      end: { x: x + width, y: currentY },
+      thickness: 1,
+      color: rgb(0, 0, 0),
+    });
+
+    const headerTexts = ['Asortyment', 'Waga Brutto', 'Pojemniki', 'Palety', 'Waga Netto', 'Waga FV'];
+    headerTexts.forEach((headerText, columnIndex) => {
+      const textX = x + cellMargin + columnWidth * columnIndex;
+      const textY = y + cellMargin - 8;
+      page.drawText(headerText, { x: textX, y: textY });
+    });
+  }
+
+  async function drawTableRows(page: any, table: any, pdfDoc: PDFDocument) {
+    const { x, y, width, height, numberOfRows, numberOfColumns, cellMargin } = table;
+    const columnWidth = width / numberOfColumns;
+  
+    let currentY = y - cellMargin - 25;
+
+    const fontB = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+    page.setFont(fontB)
+
+    items.forEach((item) => {
+      const rowValues = [
+        item.name,
+        item.weightBrutto.toString(),
+        item.containers.toString(),
+        item.pallets.toString(),
+        item.weightNetto.toString(),
+        item.weightFV.toString(),
+      ];
+      
+  
+      rowValues.forEach((value, columnIndex) => {
+        const textX = x + cellMargin + columnWidth * columnIndex;
+        page.drawText(value, { x: textX, y: currentY });
+      });
+  
+      currentY -= cellMargin + 15;
+    });
+
+    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    page.setFont(font)
+  
+    const sumRow = calculateSumRow();
+    const sumRowValues = ['Suma', ...sumRow];
+  
+    page.drawLine({
+      start: { x, y: currentY + 10 },
+      end: { x: x + width, y: currentY + 10},
+      thickness: 1,
+      color: rgb(0, 0, 0),
+    });
+  
+    sumRowValues.forEach((value, columnIndex) => {
+      const textX = x + cellMargin + columnWidth * columnIndex;
+      page.drawText(value, { x: textX, y: currentY - cellMargin - 2 });
+    });
+  }
+
+  function calculateSumRow(): string[] {
+    const sumRow: number[] = Array(5).fill(0);
+  
+    items.forEach((lineItem) => {
+      sumRow[0] += +lineItem.weightBrutto;
+      sumRow[1] += +lineItem.containers;
+      sumRow[2] += +lineItem.pallets;
+      sumRow[3] += +lineItem.weightNetto;
+      sumRow[4] += +lineItem.weightFV;
+    });
+    return sumRow.map((value) => value.toString());
+  }
+
+  async function downloadTable(): Promise<void> {
+    await createFile()
   }
 
   return (
@@ -96,8 +216,9 @@ function WeightForm() {
           {showFV ? <Table.Cell>{items.reduce((accumulator, item) => accumulator + (+item.weightFV), 0)}</Table.Cell> : null}
         </Table.Footer>
       </Table>
-      <Container fluid style={{display: 'flex', justifyContent: 'right'}}>
-        <Button.Group>
+      <Container fluid style={{display: 'flex', justifyContent: 'space-between'}}>
+        <Button icon='save' color='blue' disabled={items.length <= 0} onClick={downloadTable}/>
+        <Button.Group >
           <Button icon='eye' positive disabled={showFV} onClick={() => setShowFV(true)}/>
           <Button icon='eye slash' negative disabled={!showFV} onClick={() => setShowFV(false)}/>
         </Button.Group>
